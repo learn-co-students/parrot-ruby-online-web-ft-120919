edaf539a3e7b56acf96be6dbfc6fd3984c5fff04
adrian mohabir <mohabiradrian@gmail.com>
2021-01-17 22:46:46 -0500

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,13 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+# def parrot(string = "Squawk!")
+#   puts "#{string}"
+#   return string
+# end 
+
 def parrot(string = "Squawk!")
   puts "#{string}"
   return string
 end 
+
+# testing if lab working 
